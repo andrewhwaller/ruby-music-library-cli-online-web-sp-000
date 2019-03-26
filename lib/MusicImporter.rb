d53985314ffa5217bc664610bf61c1e32d476585
Andrew Waller <andrewhwaller@gmail.com>
2019-03-26 02:27:43 +0000

    Automatically backed up by Learn

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -7,7 +7,7 @@ class MusicImporter
   end
 
   def files
-    @files ||= Dir.glob("#{path}/*.mp3").collect{ |path| path.gsub("#{filepath}/", "") }
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |path| path.gsub("#{path}/", "") }
   end
 
   def import
