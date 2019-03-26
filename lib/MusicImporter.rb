ec3b8a3523fbe94c0296db6b7f065ca643184757
Andrew Waller <andrewhwaller@gmail.com>
2019-03-26 02:25:58 +0000

    Automatically backed up by Learn

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -1,16 +1,16 @@
 class MusicImporter
 
-  @@all = []
+  attr_reader :filepath
 
-  def initialize(file_path)
-
+  def initialize(filepath)
+    @filepath = filepath
   end
 
   def files
-    @@all
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |path| path.gsub("#{path}/", "") }
   end
 
   def import
-
+    files.each { |file| Song.create_from_filename(file) }
   end
 end
